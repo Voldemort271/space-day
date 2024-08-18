c40417b048cb629fadc8d0549fb7f3b55e2964b0
Shubham <shubhamspadhi@gmail.com>
2024-08-18 12:32:32 +0530

    Updated navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,14 +3,11 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="w-full px-12 py-5 text-lg flex justify-center sm:justify-between items-center gap-12 bg-foreground/[0.5] backdrop-blur-md">
-      <div className="hidden sm:block text-2xl font-bold">Brand</div>
-      <div className="flex gap-5">
-        <Link href={"#about"}>About</Link>
-        <Link href={"#events"}>Schedule</Link>
-        <Link href={"#speakers"}>Speakers</Link>
-        <Link href={"#register"}>Register</Link>
-      </div>
+    <div className="w-full px-12 py-5 text-lg flex justify-center items-center gap-5 sm:gap-12 lg:gap-24 bg-foreground/[0.5] backdrop-blur-md">
+      <Link href={"#about"}>About</Link>
+      <Link href={"#events"}>Schedule</Link>
+      <Link href={"#speakers"}>Speakers</Link>
+      <Link href={"#register"}>Register</Link>
     </div>
   );
 };
